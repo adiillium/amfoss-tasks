988a861928d0e91c27847e90202511b9933ed6dd
aswin c <realc@protonmail.com>
2019-07-29 19:20:29 +0530

    Diagonal Difference
    Diagonal Difference

---
+++ task-2/diagonal difference/diagonal-difference.py
@@ -0,0 +1,40 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'diagonalDifference' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY arr as parameter.
+#
+
+def diagonalDifference(arr):
+    matrix_dimension = len(arr)
+    left_to_right_diagonal_sum = 0
+    right_to_left_diagonal_sum = 0
+    for i in range(matrix_dimension):
+        left_to_right_diagonal_sum += arr[i][i]
+        right_to_left_diagonal_sum += arr[i][matrix_dimension - i - 1]
+    return abs(left_to_right_diagonal_sum - right_to_left_diagonal_sum)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = []
+
+    for _ in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = diagonalDifference(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
+
