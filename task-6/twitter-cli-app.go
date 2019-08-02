e30969338a4e0320ccefb1f31083e6f5b7a5faf0
Aswin C <realc@protonmail.com>
2019-08-02 23:26:08 +0530

    Update twitter-cli-app.go

--- task-6/twitter-cli-app.go
+++ task-6/twitter-cli-app.go
@@ -10,9 +10,9 @@ import (
 
 func main() {
 	handlePtr := flag.String("handle", "TwitterAPI", "Twitter handle")
-	api-client := anaconda.NewTwitterApiWithCredentials("93374758-MEuv7Zv4GWQkKZV7L6cTKBXJJmAMNxdJl6mKN6AFs", "NhHZ7YTEs6GwmrJ8n58hP6QCjAoVZV0r4vIpM2yprO9Vc", "3xeTO8kCsuUOonGFBpKCz4UD0", "P5h7NcN5VoQwWEeUDMkeUsB4xebtmwxAiQIx7zXrS79JPAOnig")
+	api_client := anaconda.NewTwitterApiWithCredentials("93374758-MEuv7Zv4GWQkKZV7L6cTKBXJJmAMNxdJl6mKN6AFs", "NhHZ7YTEs6GwmrJ8n58hP6QCjAoVZV0r4vIpM2yprO9Vc", "3xeTO8kCsuUOonGFBpKCz4UD0", "P5h7NcN5VoQwWEeUDMkeUsB4xebtmwxAiQIx7zXrS79JPAOnig")
 	var user anaconda.User
-	user, _ = api-client.GetUsersShow(*handlePtr, nil)
+	user, _ = api_client.GetUsersShow(*handlePtr, nil)
 	userJSON, _ := json.MarshalIndent(user, "", " ")
 	//fmt.Println(string(userJSON))
 	f, _ := os.Create("./output.txt")
