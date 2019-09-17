e19f058b8fce0e62e360924fc61e09aef4ad3548
Aswin C <realc@protonmail.com>
2019-09-17 20:14:01 +0530

    Update twitter-cli-app.go

--- task-6/twitter-cli-app.go
+++ task-6/twitter-cli-app.go
@@ -10,7 +10,7 @@ import (
 
 func main() {
 	handlePtr := flag.String("handle", "TwitterAPI", "Twitter handle")
-	api_client := anaconda.NewTwitterApiWithCredentials("93374758-MEuv7Zv4GWQkKZV7L6cTKBXJJmAMNxdJl6mKN6AFs", "NhHZ7YTEs6GwmrJ8n58hP6QCjAoVZV0r4vIpM2yprO9Vc", "3xeTO8kCsuUOonGFBpKCz4UD0", "P5h7NcN5VoQwWEeUDMkeUsB4xebtmwxAiQIx7zXrS79JPAOnig")
+	api_client := anaconda.NewTwitterApiWithCredentials("<your token>", "<your token>", "<your token>", "<your token>")
 	var user anaconda.User
 	user, _ = api_client.GetUsersShow(*handlePtr, nil)
 	userJSON, _ := json.MarshalIndent(user, "", " ")
